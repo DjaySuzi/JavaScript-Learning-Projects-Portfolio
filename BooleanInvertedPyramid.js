7516a4b88a8e70bbb42389b1612bf9740bae5acb
DjaySuzi <66435533+DjaySuzi@users.noreply.github.com>
2024-09-22 05:22:40 -0700

    Boolean Inverted Pyramid
    Change inverted variable boolean state to make the pyramid upright vs inverted.

---
+++ BooleanInvertedPyramid.js
@@ -0,0 +1,25 @@
+//change inverted vaiable to true to invert the pyramid
+const character = "D";
+const count = 9;
+const rows = [];
+let inverted = false;
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+for (let i = 1; i <= count; i++) {
+  if (inverted) {
+    rows.unshift(padRow(i, count));
+  } else {
+    rows.push(padRow(i, count));
+  }
+}
+
+let result = "";
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
