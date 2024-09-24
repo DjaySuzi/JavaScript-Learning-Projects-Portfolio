bd4a203e04172d508cb943e30381eacc45ee4a0a
DjaySuzi <66435533+DjaySuzi@users.noreply.github.com>
2024-09-24 01:29:22 -0700

    Student Final Grade Msg

--- StudentFinalGradeMsg.js
+++ StudentFinalGradeMsg.js
@@ -1,11 +1,11 @@
 //function used to calc class score average
-function getAverage(scores) {
+const getAverage = scores => {
   let sum = 0;
   for (const score of scores) {
     sum += score;
   }
   return sum / scores.length;
-}
+};
 
 //switch function converts score to grade
 const getGrade = score => {
@@ -26,20 +26,20 @@ switch (true) {
 };
 
 //boolean function for passing
-function hasPassingGrade(score) {
+const hasPassingGrade = score => {
   return getGrade(score) !== 'F';
-}
+};
 
 //1. student personalized message based on results. Use prior functions in message.
 //2. use getAverage function for total scores and getGrade function for student score. Make these paraments to student message function.
 //3. studentMsg = Class average: getAverage(totalScores). Your grade: getGrade(studentScore). You passed / failed the course.
-function studentMsg(totalScores, studentScore) {
+const studentMsg = (totalScores, studentScore) => {
 if (hasPassingGrade(studentScore)){
   return `Class average: ${getAverage(totalScores)}. Your grade: ${getGrade(studentScore)}. You passed the course.`;
 } else {
   return `Class average: ${getAverage(totalScores)}. Your grade: ${getGrade(studentScore)}. You failed the course.`;
 }
-}
+};
 
 //test via log student message function with array of total scores , student individual score
 console.log(studentMsg([92, 88, 12, 77, 57, 100, 67, 38, 97, 89], 37));
